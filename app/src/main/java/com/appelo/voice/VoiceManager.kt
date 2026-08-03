1dd069bcf205137e6e9ae2f9dd05c403ec4ba519
Ahamed Rahim <ahamedrahim023@gmail.com>
2026-08-03 21:09:46 +0600

    Create VoiceManager class with listening and speaking methods

---
+++ app/src/main/java/com/appelo/voice/VoiceManager.kt
@@ -0,0 +1,18 @@
+package com.appelo.voice
+
+class VoiceManager {
+
+    fun startListening() {
+
+        // Speech To Text পরে add হবে
+
+    }
+
+    fun speak(
+        text: String
+    ) {
+
+        // Text To Speech পরে add হবে
+
+    }
+}
