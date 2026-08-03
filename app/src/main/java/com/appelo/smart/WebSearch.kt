44937242320c6ab40a005fd0d22a1a74ab3897f8
Ahamed Rahim <ahamedrahim023@gmail.com>
2026-08-03 21:39:11 +0600

    Add WebSearch class with search function

---
+++ app/src/main/java/com/appelo/smart/WebSearch.kt
@@ -0,0 +1,17 @@
+package com.appelo.smart
+
+
+class WebSearch {
+
+
+    suspend fun search(
+        query: String
+    ): String {
+
+
+        // Real-time search API পরে add হবে
+
+
+        return "Search result for: $query"
+    }
+}
